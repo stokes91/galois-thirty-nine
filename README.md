869330d6f15fcb057f20353238c35a41cd4c4c81
Alexander Stokes <stokes91@gmail.com>
2021-01-07 12:22:51 -0700

    NPM ready

---
+++ README.md
@@ -0,0 +1,28 @@
+Using a linear feedback shift register is a useful alternative to counting with digits when a larger character set is available.
+This linear feedback shift-register with 39 bits counts to 549755813887, and can be displayed as a 14x14 datamatrix code.
+
+## Example
+```
+const GaloisLFSRThirtyNine = require('../main');
+
+const lfsr = GaloisLFSRThirtyNine.from('321FACE0FF');
+
+for (let l = 10; l--;) {
+  lfsr.iterate();
+  console.log(lfsr.toString());
+}
+```
+
+## Expected Result
+```
+ZVZRKTEB
+XVXM6UZ2
+WVWEJVX9
+WNO35VOK
+FE89PNFD
+KZRKTEBZ
+MXM6UZ2X
+NWEJVX9W
+EO35VOKO
+J89PNFD8
+```
---
+++ main.js
@@ -0,0 +1,74 @@
+/*
+   Copyright 2020 Alexander Stokes
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+const AlphaNumeric = 'WXYZBCDEFGHJKLMNO123456789PQRTUV';
+
+class GaloisLFSRThirtyNine {
+  constructor() {
+    this.lowBits = 0;
+    this.highBits = 1;
+  }
+
+  iterate() {
+    // 39 35
+    const bit = ((this.lowBits >>> 0) ^ (this.lowBits >>> 4)) & 1;
+
+    this.lowBits = (((this.highBits & 1) << 31) | (this.lowBits >>> 1)) >>> 0;
+    this.highBits = (bit << 6) | (this.highBits >>> 1);
+
+    return this;
+  }
+
+  toString() {
+    const r = [
+      this.highBits >>> 3,
+      this.lowBits,
+      this.lowBits >>> 5,
+      this.lowBits >>> 10,
+      this.lowBits >>> 15,
+      this.lowBits >>> 20,
+      this.lowBits >>> 25,
+      (this.highBits << 2) | (this.lowBits >>> 30)
+    ];
+
+    return r.map((b) => {
+      b &= 0x1F;
+      return AlphaNumeric[b];
+    }).join('');
+  }
+
+  toDBString() {
+    const buf = Buffer.allocUnsafe(5);
+    buf.writeUInt8(this.highBits, 0);
+    buf.writeUInt32BE(this.lowBits, 1);
+    return buf.toString('hex');
+  }
+}
+
+GaloisLFSRThirtyNine.from = (input) => {
+  if (!/[\da-f]{10}/gi.test(input)) {
+    return false;
+  }
+
+  const lfsr = new GaloisLFSRThirtyNine();
+
+  const buf = Buffer.from(input, 'hex');
+  lfsr.highBits = buf.readUInt8(0);
+  lfsr.lowBits = buf.readUInt32BE(1);
+  return lfsr;
+};
+
+module.exports = GaloisLFSRThirtyNine;
